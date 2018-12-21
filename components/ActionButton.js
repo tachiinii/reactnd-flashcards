4dc0bfd86b7ca79daa0d257bb74e132c33016a92
Colen Wilson <colen.wilson@domail.maricopa.edu>
2018-12-21 01:16:55 -0700

    Add AddDeck form components

---
+++ components/ActionButton.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Text, Platform, TouchableOpacity, StyleSheet } from 'react-native'
+
+export default function ActionButton({ label, onPress }) {
+  return (
+    <TouchableOpacity
+      style={Platform.OS === 'ios' ? styles.iosButton : styles.androidButton}
+      onPress={onPress}>
+      <Text style={styles.buttonText}>{ label }</Text>
+    </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+  iosButton: {
+    backgroundColor: '#757575',
+    padding: 10,
+    borderRadius: 7,
+    height: 45,
+    marginLeft: 40,
+    marginRight: 40,
+  },
+  androidButton: {
+    backgroundColor: '#757575',
+    padding: 10,
+    borderRadius: 2,
+    height: 45,
+    paddingLeft: 30,
+    paddingRight: 30,
+    alignSelf: 'flex-end',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#ffffff',
+    fontSize: 22,
+    textAlign: 'center',
+  }
+})
--- components/AddDeck.js
+++ components/AddDeck.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, TextInput, StyleSheet } from 'react-native'
+import ActionButton from './ActionButton'
 
 export default class AddDeck extends Component {
+  state = {
+    input: 'Placeholder'
+  }
+
+  handleTextChange = (input) => {
+    this.setState({
+      input
+    })
+  }
+
+  handleSubmit = () => {
+  }
+
   render() {
-    return <View>
-      <Text>AddDeck</Text>
-    </View>
+    return (
+      <View>
+        <TextInput
+          style={styles.textField}
+          value={this.state.input}
+          onChangeText={this.handleTextChange}
+        />
+        <ActionButton label='Create Deck' onPress={this.handleSubmit} />
+      </View>
+    )
   }
 }
+
+const styles = StyleSheet.create({
+  textField: {
+    width: 200,
+    height: 44,
+    padding: 8,
+    borderWidth: 1,
+    borderColor: '#757575',
+    margin: 50
+  }
+})
